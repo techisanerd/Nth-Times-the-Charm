0c325d220fea358789d70d3a097416cae71eec61
Estella A <e.stellaa1132@gmail.com>
2025-11-10 15:12:57 -0800

    add user read, write

--- DataManager.py
+++ DataManager.py
@@ -1,13 +1,14 @@
 from Classes import *
 from pathlib import Path
 
-import json, datetime
+import json, os
 
 class DataManager():
     __instance = None
     #moviesFolder is the path to the Movies folder
     moviesFolder = Path(__file__).resolve().parent / "Movies"
-
+    #path to json file storing user data 
+    userFile = ".\\users.json"
     #init raises an error since this is a singleton
     def __init__(self):
         raise RuntimeError("This Object cannot be made with this function, please use getInstance")
@@ -46,11 +47,34 @@ class DataManager():
     def deleteMovie():
         pass
 
-    def createUser():
-        pass
+    def createUser(self, name:str, email:str, profilePic:str, passwordHash:str, auth):
+        usr = User(name, email, profilePic, passwordHash, auth)
 
-    def readUser():
-        pass
+        userList = [] 
+        if os.path.exists(self.userFile):
+            try:
+                with open(self.userFile, "r") as f:
+                    userList = json.load(f)
+            except:
+                print("something has gone wrong reading json file.")
+
+        userList.append(usr)
+    
+        with open(self.userFile, "w") as file:
+            #store as a json: each user is converted to a dict(ionary) of its attributes, within the list
+            json.dump([user.__dict__ for user in userList], file, indent=4)
+
+    # refactor candidate: move to usermanager
+    def readUser(self, name:str):
+        userList = self.getUsers()
+        for u in userList:
+            if u.name == name:
+                return u 
+
+        #if user does not exist return nothing
+        return None
+
+
     
     def updateUser():
         pass
@@ -88,10 +112,22 @@ class DataManager():
         pass
 
     # get list of all users in database
-    def getMovies():
-        pass
+    def getUsers(self):
+        dictList = [] 
+        if os.path.exists(self.userFile):
+            with open(".\\users.json", "r") as f:
+                dictList = json.load(f)
+
+        #deserialize: create new object for each user in the json dictionary
+        userList = [User(**userData) for userData in dictList]
+        return userList
 
     # get all reports in database
     def getReports():
         pass
 
+# temp
+if __name__ == "__main__":
+    inst = DataManager.getInstance()
+    inst.createUser("dorothy", "asdf","link goes here", "hash", None)
+    print(inst.readUser("dorothy"))
---
+++ Managers.py
@@ -0,0 +1,15 @@
+import DataManager
+
+class UserManager():
+    def readUser():
+
+        pass
+    def getUsers():
+        pass
+    def createUser():
+        pass
+    def updateUser():
+        pass
+    def deleteUser():
+        pass
+
