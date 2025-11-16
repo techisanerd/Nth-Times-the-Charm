b2099c916260e50ae515f197abc7229bf555a4d9
MarsIsAGreatPlanet <maragaret.a.tilley@gmail.com>
2025-11-15 21:21:46 -0800

    added tests to user and review Controllers

--- Controllers.py
+++ Controllers.py
@@ -1,59 +1,67 @@
 import hashlib
 from fastapi import HTTPException
-from DataManager import *
-import uuid
+from datetime import date
+from DataManager import DataManager
+from Managers import UserManager
+from Managers import ReviewManager
+from Classes import Review
+from Classes import User
 
 class UserController():
 
-    def createUser(username:str, email:str, password:str):
-        """Create a new user profile.
-          To add profile pic call addProfilePic(username, url)"""
-        hashedPassword = hashPassword(password)
-        UserManager.createUser(username,email,password)
-
-    def addProfilePic(username:str, url:str):
-        """Add a profile picture. Requires username and the url of the picture being added"""
-        User = UserManager.getUser(username)
-        UserManager.updateUser(username, User.email, url)
+    def createUser(username:str, email:str, profilePic:str, password:str,):
+        if(UserManager.readUser(username)!=None):
+            raise HTTPException(status_code = 400, detail = "400 Username already in use")
+        if(len(password)<8):
+            raise HTTPException(status_code = 400, detail = "400 Password should be 8 or more characters")
+        hashedPassword = UserController.hashPassword(password)
+        UserManager.createUser(username,email,profilePic,hashedPassword.hexdigest())
     
     def hashPassword(passwordPlaintext:str) -> hashlib.sha224:
-        return hashlib.sha224(passwordPlaintext, True)
+        return hashlib.sha224(passwordPlaintext.encode(), usedforsecurity= True)
 
 class ReviewController():
 
-    def addReview(username:str, rating:int, title:str,description:str):
-        if(UserManager.getUser(username) is None):
+    def addReview(movie:str, username:str, rating:int, title:str,description:str):
+        if(MovieManager.readMovie(movie) is None):
+            raise HTTPException(statuscode = 404, detail = "404 Movie not found")
+        if(UserManager.readUser(username) is None):
             raise HTTPException(statuscode = 404, detail = "404 User not found")
         if(rating >10 | rating <0):
             raise HTTPException(statuscode = 400, detail = "400 Rating needs to be an integer between 0 and 10")
-        new_id = str(uuid.uuid4())
-        if (ReviewManager.getReview(new_id) is None):
-            UserManager.createReview(new_id, username,rating,title,description)
+        reviewList = ReviewManager.readReview(movie,username) 
+        reviewList = [r for r in reviewList if r.title == title]
+        if (reviewList == []):
+            UserManager.createReview(movie, date.today, username,0,0,rating,title,description)
         else:
-            raise HTTPException(status_code=409, detail="ID collision; retry.")
+            raise HTTPException(status_code=409, detail="409 Review with this username and title already exists for this movie")
     
-    def editReview(reviewId:str,rating:int,title:str,description:str):
-        review = ReviewManager.getReview(reviewId)
-        if(review is None):
-            raise HTTPException(statuscode = 404, detail = "404 Review '{reviewId}' not found")
+    def editReview(movie:str, username:str, rating:int, title:str, newTitle:str, description:str):
+        reviewList = ReviewManager.readReview(movie,username) 
+        reviewList = [r for r in reviewList if r.title == title]
+        if(reviewList == []):
+            raise HTTPException(statuscode = 404, detail = "404 Review '{title}' not found")
         if(rating >10 | rating <0):
             raise HTTPException(statuscode = 400, detail = "400 Rating needs to be an integer between 0 and 10")
-        ReviewManager.updateReview(reviewId, review.rating if rating is None else rating, review.title if title is None else title,
-                                    review.description if description is None else description)
+        for r in reviewList:
+            ReviewManager.updateReview(movie,r,date.now,username,0,0,rating,newTitle,description)
 
-    def removeReview(reviewId:str):
-        review = ReviewManager.getReview(reviewId)
-        if(review is None):
-            raise HTTPException(statuscode = 404, detail = "404 Review '{reviewId}' not found")
-        ReviewManager.deleteReview(reviewId)
+    def removeReview(movie:str, username:str,title:str):
+        reviewList = ReviewManager.readReview(movie,username) 
+        reviewList = [r for r in reviewList if r.title == title]
+        if(reviewList == []):
+            raise HTTPException(statuscode = 404, detail = "404 Review '{title}' not found")
+        for r in reviewList:
+            ReviewManager.deleteReview(movie, r)
 
-    def searchByName(title:str) -> Review:
+    def searchByName(title:str):
         reviews = DataManager.getReviews()
+        foundReviews = []
         for r in reviews:
-            if (r.title.toLower() == title.toLower()):
-                return  r
-        raise HTTPException(statuscode = 404, detail = "404 Review '{reviewId}' not found")
-    
+            if (title.toLower() in r.title.toLower()):
+                foundReviews.append(r)
+        return foundReviews
+
 
 
         
--- Tests.py
+++ Tests.py
@@ -1,6 +1,11 @@
 import pytest
 from DataManager import DataManager
 from Managers import UserManager
+from Controllers import UserController
+from Controllers import ReviewController
+from Classes import Review
+from datetime import date
+from fastapi import HTTPException
 def testSingleton():
     """Testing that we only get one instance of DataManager"""
     dataManager1 = DataManager.getInstance()
@@ -20,3 +25,60 @@ def testUpdateUser():
     v = UserManager.updateUser(u, name="NEWTESTUSER")
     UserManager.deleteUser("NEWTESTUSER")
     assert UserManager.readUser("TESTUSER") == None and v.name == "NEWTESTUSER"
+
+def testUserCreation():
+    UserController.createUser("TestUser","mail@example.com","https://profilepic.example.com","PlainTestPassword")
+    u = UserManager.readUser("TestUser")
+    hashPassword = UserController.hashPassword("PlainTestPassword").hexdigest()
+    UserManager.deleteUser("TestUser")
+    assert u.name == "TestUser" and u.email == "mail@example.com" and u.profilePic == "https://profilepic.example.com"
+    assert u.passwordHash == hashPassword
+
+def testRepeatUsername():
+    UserManager.createUser("TestUser","mail@example.com","https://profilepic.example.com","0xabcdef")
+    with pytest.raises(HTTPException) as HTTPError:
+        UserController.createUser("TestUser","mail@example.com","https://profilepic.example.com","PlainTestPassword")
+    UserManager.deleteUser("TestUser")
+    assert "Username already in use" in str(HTTPError.value)
+
+def testTooShortPassword():
+    with pytest.raises(HTTPException) as HTTPError:
+        UserController.createUser("TestUser","mail@example.com","https://profilepic.example.com","tiny")
+    UserManager.deleteUser("TestUser")
+    assert "Password should be 8 or more characters" in str(HTTPError.value)
+
+def testAddReviewInvalidUser():
+    with pytest.raises(HTTPException) as HTTPError:
+        ReviewController.addReview("Joker","Non-Existant-User",0,"hi","hi")
+    assert "User not found" in str(HTTPError.value)
+
+def testAddReviewInvalidMovie():
+    with pytest.raises(HTTPException) as HTTPError:
+        ReviewController.addReview("Non-Existant-Movie","User",0,"hi","hi")
+    assert "Movie not found" in str(HTTPError.value)
+
+def testAddReviewInvalidRating():
+    with pytest.raises(HTTPException) as HTTPError:
+        UserController.createUser("TestUser","mail@example.com","https://profilepic.example.com","PlainTextPassword")
+        ReviewController.addReview("Joker","TestUser",11,"hi","hi")
+        UserManager.deleteUser("TestUser")
+    assert "Rating needs to be an integer between 0 and 10" in str(HTTPError.value)
+
+def testEditReview():
+    ReviewController.addReview("Joker","TestUser",7,"hi","hi")
+    ReviewController.editReview("Joker","TestUser",7,"hi","NEW TITLE","NEW DESCRIPTION")
+    reviewList = ReviewManager.readReview("Joker","TestUser")
+    for r in reviewList:
+        assert r.title == "NEW TITLE" and r.description == "NEW DESCRIPTION"
+        ReviewController.removeReview("Joker","TestUser","NEW TITLE")
+
+def testSearchReviews():
+    ReviewController.addReview("Joker","TestUser",7,"hi","hi")
+    ReviewController.addReview("Joker","TestUser2",7,"no","hi")
+    reviewList = ReviewController.searchByName("H")
+    reviewTitles = []
+    ReviewController.removeReview("Joker","TestUser","hi")
+    ReviewController.removeReview("Joker","TestUser2","no")
+    for r in reviewList:
+        reviewTitles.append(r.title)
+    assert "hi" in reviewTitles and "no" not in reviewTitles
