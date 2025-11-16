297daea3b20a3586fe13ed8f53692d6bfb0c3dbd
treeturtle <owenstrolle@gmail.com>
2025-11-15 23:39:29 -0500

    createMovie() and test

--- Tests.py
+++ Tests.py
@@ -1,6 +1,11 @@
 import pytest
+import json
+import datetime
 from DataManager import DataManager
 from Managers import UserManager
+from Classes import Movie 
+from pathlib import Path
+
 def testSingleton():
     """Testing that we only get one instance of DataManager"""
     dataManager1 = DataManager.getInstance()
@@ -20,3 +25,61 @@ def testUpdateUser():
     v = UserManager.updateUser(u, name="NEWTESTUSER")
     UserManager.deleteUser("NEWTESTUSER")
     assert UserManager.readUser("TESTUSER") == None and v.name == "NEWTESTUSER"
+
+#tests for movie manager functions
+
+#create temp movie directory for testing 
+@pytest.fixture
+def tempMoviesFolder(tmp_path):
+    folder = tmp_path / "tempMovies"
+    folder.mkdir()
+
+    dm = DataManager.getInstance()
+    dm.moviesFolder = folder
+    return dm
+
+#movie object for tests
+def sampleMovie():
+    return Movie(
+        title="Test Movie",
+        rating=7.5,
+        ratingCount=1500,
+        userReviews=300,
+        criticReviews=50,
+        metaScore=65,
+        genres=["Drama", "Thriller"],
+        directors=["Jane Doe"],
+        dateReleased=datetime.date(2020, 5, 20),
+        creators=["John Smith"],
+        actors=["Actor A", "Actor B"],
+        description="A test movie for unit testing.",
+        duration=125
+    )
+
+#test creating a movie
+def testCreateMovie(tmpMoviesFolder):
+    dm = tmpMoviesFolder
+    movie = sampleMovie()
+
+    created = dm.createMovie(movie)
+    assert created is True
+    filepath = dm.moviesFolder / "Test_Movie.json"
+    assert filepath.exists()
+
+    #check to see if data is correct
+    with open(filepath, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    
+    assert data["title"] == "Test Movie"
+    assert data["movieIMDbRating"] == 7.5
+    assert data["totalRatingCount"] == 1500
+    assert data["totalUserReviews"] == 300
+    assert data["totalCriticReviews"] == 50
+    assert data["metaScore"] == 65
+    assert data["movieGenres"] == ["Drama", "Thriller"]
+    assert data["directors"] == ["Jane Doe"]
+    assert data["datePublished"] == "2020-05-20"
+    assert data["creators"] == ["John Smith"]
+    assert data["mainStars"] == ["Actor A", "Actor B"]
+    assert data["description"] == "A test movie for unit testing."
+    assert data["duration"] == 125
