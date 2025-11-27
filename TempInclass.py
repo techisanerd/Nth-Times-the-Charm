99fd6e159ac7d67f08b1a95ffc4ab8ec2017615b
Margaret <margaret.a.tilley@gmail.com>
2025-11-27 08:54:17 -0800

    Update TempInclass.py

--- TempInclass.py
+++ TempInclass.py
@@ -1,4 +1,4 @@
-repo = Path(__file__).resolve().parent.parent
+repo = Path(__file__).resolve().parent
 for commit in Repository('repo').traverse_commits():
     for file in commit.modified_files:
         print('Author {} modified {} in commit {}'.format(commit.author.name, file.filename, commit.hash))
