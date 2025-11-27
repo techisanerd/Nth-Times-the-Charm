42eb1b5fafbf151f8afacca0ebf368e8863d5b8a
Margaret <margaret.a.tilley@gmail.com>
2025-11-27 08:58:07 -0800

    Update TempInclass.py

--- TempInclass.py
+++ TempInclass.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import PyDriller
 repo = Path(__file__).resolve().parent
-for commit in Repository('repo').traverse_commits():
+for commit in Repository(repo).traverse_commits():
     for file in commit.modified_files:
         print('Author {} modified {} in commit {}'.format(commit.author.name, file.filename, commit.hash))
